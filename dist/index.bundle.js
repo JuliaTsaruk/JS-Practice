598c3339fa7cf0ec6f88fac24acd07b2d8e8749a
Yuliya Tsaruk <yuliyatsaruk@yandex.ru>
2021-05-20 18:56:32 +0300

    feature/JS-1: webpack in process

---
+++ dist/index.bundle.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";console.log("gghghgh"),alert("aaoooaa")})();
--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
 import "../styles/style.scss";
+import "../assets/img/earth.jpg"
 
 console.log("gghghgh");
-alert("aaaaa");
+alert("aaoooaa");
